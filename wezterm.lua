6ccf10bd25fdf277e25d8865dab9b114c2c300f4
Yugm Shah <yugsha28@gmail.com>
2025-04-20 16:32:29 -0400

    fix some errors

--- wezterm.lua
+++ wezterm.lua
@@ -49,54 +49,64 @@ end
 -- keybindings
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  -- Send C-a when pressing C-a twice
-  { key = "a", mods = "LEADER",       action = act.SendKey { key = "a", mods = "CTRL" } },
-  { key = "c", mods = "LEADER",       action = act.ActivateCopyMode },
+    -- Send C-a when pressing C-a twice
+    { key = "a", mods = "LEADER",       action = act.SendKey { key = "a", mods = "CTRL" } },
+    { key = "c", mods = "LEADER",       action = act.ActivateCopyMode },
 
-  -- Pane keybindings
-  { key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-  -- SHIFT is for when caps lock is on
-  { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-  { key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
-  { key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
-  { key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
-  { key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
-  { key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
-  { key = "z", mods = "LEADER",       action = act.TogglePaneZoomState },
-  { key = "s", mods = "LEADER",       action = act.RotatePanes "Clockwise" },
-  -- We can make separate keybindings for resizing panes
-  -- But Wezterm offers custom "mode" in the name of "KeyTable"
-  { key = "r", mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+    -- Pane keybindings
+    { key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+    -- SHIFT is for when caps lock is on
+    { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
+    { key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
+    { key = "z", mods = "LEADER",       action = act.TogglePaneZoomState },
+    { key = "s", mods = "LEADER",       action = act.RotatePanes "Clockwise" },
+    -- We can make separate keybindings for resizing panes
+    -- But Wezterm offers custom "mode" in the name of "KeyTable"
+    { key = "r", mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
 
-  -- Tab keybindings
-  { key = "n", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
-  { key = "[", mods = "LEADER",       action = act.ActivateTabRelative(-1) },
-  { key = "]", mods = "LEADER",       action = act.ActivateTabRelative(1) },
-  { key = "t", mods = "LEADER",       action = act.ShowTabNavigator },
-  -- Key table for moving tabs around
-  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+    -- Tab keybindings
+    { key = "n", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "[", mods = "LEADER",       action = act.ActivateTabRelative(-1) },
+    { key = "]", mods = "LEADER",       action = act.ActivateTabRelative(1) },
+    { key = "t", mods = "LEADER",       action = act.ShowTabNavigator },
+    -- Key table for moving tabs around
+    { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
 
 
-  -- Lastly, workspace
-  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+    -- Lastly, workspace
+    { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
 
-  -- rename tab
-  {
+    -- rename tab
+    {
     key = 'e',
     mods = "LEADER",
     action = act.PromptInputLine {
-      description = 'Enter new name for tab',
-      action = wezterm.action_callback(function(window, pane, line)
+        description = 'Enter new name for tab',
+        action = wezterm.action_callback(function(window, pane, line)
         -- line will be `nil` if they hit escape without entering anything
         -- An empty string if they just hit enter
         -- Or the actual line of text they wrote
         if line then
           window:active_tab():set_title(line)
         end
-      end),
+        end),
     },
-  },
+},
 
+{
+key = "c",
+mods = "CTRL|SHIFT",
+action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
+},
+{
+key = "v",
+mods = "CTRL|SHIFT",
+action = wezterm.action.PasteFrom("Clipboard"),
+},
 }
 -- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
 for i = 1, 9 do
